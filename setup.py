344640bdb8a12e3293f9215d77699590b305ba37
Antonio Ventilii <antonioventilii@gmail.com>
2024-03-27 15:07:52 +0100

    refactor: class FirestoreDB was split in smaller classes for an easier readability

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='firestore_wrapper',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     description='A custom wrapper for Google Firestore.',
     long_description=open('README.md').read(),
